a1007d6c2451aaffce235694d8da8c7a8909c14a
slowhand0309 <slowhand0309@gmail.com>
2016-01-08 01:20:58 +0900

    Added spec for ping

---
+++ spec/api/ping_spec.rb
@@ -0,0 +1,20 @@
+require "michieki"
+require "spec_helper"
+require 'open-uri'
+require 'json'
+require 'fileutils'
+
+RSpec.describe MichiEki::Ping do
+
+  it 'ping' do
+    json = open("http://michieki.example.com/api/ping").read
+    result = JSON.parse(json)
+    expect(result['ping']).to eq 'pong'
+  end
+
+  it 'echo' do
+    json = open("http://michieki.example.com/api/ping?echo=hello").read
+    result = JSON.parse(json)
+    expect(result['ping']).to eq 'pong+hello'
+  end
+end
