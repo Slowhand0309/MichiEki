76a1c5a8be926507d2e3472316da7fae634758db
slowhand0309 <slowhand0309@gmail.com>
2016-02-17 01:22:54 +0900

    Added search api spec

---
+++ spec/api/search_spec.rb
@@ -0,0 +1,24 @@
+# encoding: utf-8
+
+require "michieki"
+require "spec_helper"
+require 'open-uri'
+require 'json'
+
+RSpec.describe MichiEki::Search do
+
+  before do
+    # check exist data file
+    unless FileTest.exist?('data/station_list.json')
+      fail('please generate data file by [rake all]')
+    end
+  end
+
+  it 'search area' do
+    json = open(URI.escape("http://michieki.example.com/api/search?lat=35.68&lng=139.69")).read
+    result = JSON.parse(json)
+    expect(result.size).to eq 1
+    expect(result[0]['name']).to eq '八王子滝山'
+  end
+
+end
